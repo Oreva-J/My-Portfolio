06ab52f3e3ff35956553d8da442c3fd0a5f8fb80
oreva <orevaawusi@gmail.com>
2025-02-08 21:50:14 +0100

    All folder Structures Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
-
+import {BrowserRouter} from "react-router-dom"
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import About from './components/About'
+import Experience from './components/Experience'
+import Works from './components/Works'
+import Tech from './components/Tech'
+import Feedbacks from './components/Feedbacks'
+import Contact from './components/Contact'
+import Stars from './components/canvas/Stars'
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary'>
+        <header className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
+        </header>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className='relaive z-0'>
+          <Contact />
+          <Stars />
+        </div>
+      </div>
       
-    </div>
+    </BrowserRouter>
   )
 }
 
